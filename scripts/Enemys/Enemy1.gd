50e447eb10c0da8885d9069d2159f78c786b0eb2
Maik Schindler <maik.schindler2003@gmail.com>
2025-01-04 17:37:45 +0100

    improved enemy

--- scripts/Enemys/Enemy1.gd
+++ scripts/Enemys/Enemy1.gd
@@ -1,11 +1,25 @@
 extends CharacterBody2D
 
 
-var player : Player
+@onready var player : Player = %Player
 @onready var health_manager = $HealthManager
-
+var direction : int
+const SPEED : int = 50
 func take_damage(damage : int):
 	health_manager.take_damage(damage)
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if player.global_position.x - global_position.x < -10:
+		direction = -1
+	elif player.global_position.x - global_position.x > 10:
+		direction = 1
+	elif is_on_floor() and global_position.y > player.global_position.y and player.global_position.distance_to(global_position) < 100:
+		velocity.y = -200
+	velocity.x = lerpf(velocity.x, direction * SPEED, minf(1, 4 * delta))
+	move_and_slide()
 
 func die():
 	queue_free()
